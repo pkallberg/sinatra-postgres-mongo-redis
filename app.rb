92d9f742760c8ae67efc9272b6d6ae9553d18c4a
Philip Källberg <philip@cloud66.com>
2013-12-03 17:03:14 +0000

    Update app.rb

--- app.rb
+++ app.rb
@@ -19,14 +19,14 @@ class App < Sinatra::Application
     YAML::load(File.open('config/database.yml'))[ENV['RACK_ENV']].symbolize_keys.each do |key, value|
       set key, value
     end
-    
+
     ActiveRecord::Base.establish_connection(
-      adapter: "postgresql", 
-      host: ENV['POSTGRESQL_URL'],
+      adapter: "postgresql",
+      host: ENV['POSTGRESQL_ADDRESS'],
       database: ENV['POSTGRESQL_DATABASE'],
-      username: ENV['POSTGRESQL_USER'],
+      username: ENV['POSTGRESQL_USERNAME'],
       password: ENV['POSTGRESQL_PASSWORD'])
-    
+
     Mongoid.configure do |config|
       if ENV['MONGODB_URL']
         conn = Mongo::Connection.from_uri(ENV['MONGODB_URL'])
@@ -36,16 +36,16 @@ class App < Sinatra::Application
         config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('sinatra_app_development')
       end
     end
-    
+
   end
-  
+
   configure :development do
     enable :logging, :dump_errors, :raise_errors
-    
+
     set :cache, Dalli::Client.new
-    
+
     REDIS = Redis.new(:host => "127.0.0.1", :port => 6379, :password => "")
-    
+
     use Rack::Throttle::Hourly,   :max => 1000, :cache => REDIS, :key_prefix => 'throttle_sinatra_app'
     Resque.redis = REDIS
   end
@@ -56,29 +56,29 @@ class App < Sinatra::Application
 
   configure :production do
     enable :logging, :dump_errors, :raise_errors
-    
+
     ENV["LOG_LEVEL"] = "DEBUG"
-    
+
     set :cache, Dalli::Client.new
-    
-    uri = URI.parse(ENV["REDISTOGO_URL"])
+
+    uri = URI.parse(ENV["REDIS_ADDRESS"])
 
     REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    
+
     use Rack::Throttle::Hourly,   :max => 1000, :cache => REDIS, :key_prefix => :throttle
     Resque.redis = REDIS
-    
+
   end
-  
+
   configure :staging do
     enable :logging, :dump_errors, :raise_errors
-    
+
     set :cache, Dalli::Client.new
-    
-    uri = URI.parse(ENV["REDISTOGO_URL"])
-    
+
+    uri = URI.parse(ENV["REDIS_ADDRESS"])
+
     REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-    
+
     use Rack::Throttle::Hourly,   :max => 1000, :cache => REDIS, :key_prefix => :throttle
     Resque.redis = REDIS
   end
