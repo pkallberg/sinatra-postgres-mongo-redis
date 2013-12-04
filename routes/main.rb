b06979dc899fa4be56da877e3e17b2db22b6b223
Philip Källberg <philip@cloud66.com>
2013-12-04 14:32:36 +0000

    Update main.rb

--- routes/main.rb
+++ routes/main.rb
@@ -1,4 +1,3 @@
-
 class App < Sinatra::Application
   get '/' do
     #VARNISH
@@ -11,15 +10,20 @@ class App < Sinatra::Application
     REDIS.set("#{Time.now}", "#{Time.now}")
     #RESQUE
     Resque.enqueue(Eat, "hello")
+
+    username = [*('A'..'Z')].sample(8).join
+    lastname = [*('A'..'Z')].sample(8).join
+
     #MONGODB/MONGOID
-    u = User.create(:first_name => 'Bob', :last_name => 'Tester')
-    
+    u = User.create(:first_name => username, :last_name => lastname)
+
     #POSTGRES/ACTIVERECORD
-    a = Account.create(:name => params[:name] || "mice", :owner_id => 1234)
-    
-    "user: #{u.id}<br><br>
-    account: #{a.id || a.errors}<br><br>
-    Hello from #{color} Sinatra on Heroku! #{ENV['RACK_ENV']}, Rendered at #{Time.now}<br><br>
+    a = Account.create(:name => username || "mice", :owner_id => rand(1000))
+
+    "Successfully added #{u.first_name} to MongoDB<br><br>
+    Successfully added id: #{a.id || a.errors} to PostgreSQL <br><br>
+    Hello from #{color} Sinatra on Heroku! #{ENV['RACK_ENV']}<br><br>
+    Rendered at #{Time.now}<br><br>
     #{REDIS.keys("throttle*").map{|key| [key, REDIS.get(key)]}}<br><br>
     #{REDIS.keys("*")}<br><br>
     <a href='/resque'>resque</a>"
